4e604f250afe2fa723635b637fd7f55c88fbb429
Krishnanadimuthu <109790613+Krishnanadimuthu@users.noreply.github.com>
2023-10-13 22:20:59 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 variable "access_key" {
   description = "Controls if CloudFront distribution should be created"
